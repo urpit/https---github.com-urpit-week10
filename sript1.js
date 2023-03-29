3edae39ffa0b49ea45c859b4077ff4599db3d040
urpit <patelurpit123@gmail.com>
2023-03-29 13:26:14 -0400

    first commit

---
+++ sript1.js
@@ -0,0 +1,8 @@
+console.log("hoii");
+function math() {
+  var a = 3;
+  var b = 4;
+  var c = a + b;
+  return c;
+}
+console.log(math());
---
+++ static/script/script.js
@@ -0,0 +1,10 @@
+console.log("node js function");
+var os = require("os");
+
+console.log(__dirname);
+console.log(os.platform);
+//if we want to run express application we use following command
+//folder which has "package.json"
+//npm install: used to intall a package from npm repository so that you cause it...
+//npm prune: will look through your package.json file and remove any npm modules that are installed but not required for your project..
+//npm list: shows a list of all packages installed for use by this application...
